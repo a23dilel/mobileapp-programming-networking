124b1005986d536e142deefb0ac40c380f4fb7a1
a23dilel <a23dilel@student.his.se>
2024-04-16 11:38:28 +0200

    Added an ArrayList<Mountain> and RecyclerView adapter as a member variable member in MainActivity.java

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,10 +4,22 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
+    ArrayList<Mountain> mountains = new ArrayList<>(Arrays.asList(
+            new Mountain("Mount Fuji", 100, 200),
+            new Mountain("Mount Everest", 50, 100),
+            new Mountain("Denali", 500, 300)
+    ));
+
+    //##############################################################################################################################
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
 
@@ -16,6 +28,13 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mountains);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //##############################################################################################################################
+
         new JsonFile(this, this).execute(JSON_FILE);
     }
 
@@ -23,5 +42,4 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
     }
-
 }
